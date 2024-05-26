b6a418c12add0a5b438c70b9fadbce4de8df5625
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-05-26 16:06:01 +0200

    model packing

---
+++ model_packing.py
@@ -0,0 +1,13 @@
+import pickle
+import joblib
+import torch
+from torch.onnx import export
+from onnx_tf.backend import prepare
+from onnx import load
+import tensorflow as tf
+from tensorflow import saved_model
+
+def pack_up_model():
+    pass
+
+pack_up_model()
